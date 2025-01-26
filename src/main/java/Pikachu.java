231fad535c8891053e22f910d9bea3d214cfa602
Jensen188 <jensenteo1206@gmail.com>
2025-01-26 16:22:21 +0800

    Create a list to save and display tasks

--- src/main/java/Pikachu.java
+++ src/main/java/Pikachu.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Pikachu {
-    private static final String line = "-------------------" +
-            "-------------------";
+    private static final String line = "-------------------"
+            + "-------------------";
     private static final String name = "Pikachu";
     private static boolean isExit = false;
-
+    private static final ArrayList<String> list = new ArrayList<>();
 
     //Printing lines
     private static void line() {
         System.out.println(line);
     }
 
-    private static void start(){
+    private static void start() {
         line();
-        System.out.printf("It's me %s!!\n" +
-                "What can I do for you?\n\n", name );
+        System.out.printf("""
+                It's me %s!!
+                What can I do for you?
+                
+                """, name );
         line();
     }
 
@@ -25,22 +29,33 @@ public class Pikachu {
         line();
     }
 
-    //Repeating the command
+    //React based on command given
     private static void echo(String command) {
-        if (!command.equals("bye")) {
-            System.out.println(command);
-            line();
-        } else {
+        switch (command) {
+        case ("bye"):
             exit();
             Pikachu.isExit = true;
+            break;
+        case "list":
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%d. %s\n", i+1, list.get(i));
+            }
+            line();
+            break;
+        default:
+            list.add(command);
+            System.out.printf("added: %s\n", command);
+            line();
+            break;
         }
     }
 
     public static void main(String[] args) {
         start();
         Scanner sc = new Scanner(System.in);
-        while(!Pikachu.isExit) {
-            echo(sc.nextLine());
+        while (!Pikachu.isExit) {
+            String command = sc.nextLine();
+            echo(command);
         }
 
     }
