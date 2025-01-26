2384bcc6e0661c376baa5dc4260c343d0748b05b
Jensen188 <jensenteo1206@gmail.com>
2025-01-26 15:09:43 +0800

    Add echo function to Pikachu.java

--- src/main/java/Pikachu.java
+++ src/main/java/Pikachu.java
@@ -1,22 +1,47 @@
+import java.util.Scanner;
+
 public class Pikachu {
     private static final String line = "-------------------" +
             "-------------------";
     private static final String name = "Pikachu";
+    private static boolean isExit = false;
+
 
     //Printing lines
     private static void line() {
         System.out.println(line);
     }
 
-    public static void main(String[] args) {
+    private static void start(){
         line();
-        System.out.printf("Hello, I'm %s!\n" +
+        System.out.printf("It's me %s!!\n" +
                 "What can I do for you?\n\n", name );
         line();
-        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    //Stop the chatbot
+    private static void exit() {
+        System.out.printf("Bye. %s wants to see you again soon!\n", Pikachu.name);
         line();
+    }
 
+    //Repeating the command
+    private static void echo(String command) {
+        if (!command.equals("bye")) {
+            System.out.println(command);
+            line();
+        } else {
+            exit();
+            Pikachu.isExit = true;
+        }
+    }
 
+    public static void main(String[] args) {
+        start();
+        Scanner sc = new Scanner(System.in);
+        while(!Pikachu.isExit) {
+            echo(sc.nextLine());
+        }
 
     }
 }
