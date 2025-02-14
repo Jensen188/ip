0fe3ad91a4667799b1d747b0b094ded851de036b
Jensen188 <jensenteo1206@gmail.com>
2025-02-14 13:13:41 +0800

    Use LocalDate to handle deadline in Deadline.java

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,20 +1,24 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate deadline;
+    protected static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.deadline = LocalDate.parse(by);
     }
 
     @Override
     public String saveAsFileFormat() {
-        return "D|" + this.isDone + "|" + this.task + "|" + this.by;
+        return "D|" + this.isDone + "|" + this.task + "|" + this.deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.deadline.format(formatter) + ")";
     }
 }
